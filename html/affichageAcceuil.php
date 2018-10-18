8d2c17cdcad27b4c3b0a8aaff07ea36dc8e30ac6
alan-dauphas <alan.dauphas@me.com>
2018-10-18 15:15:45 +0200

    premier pas avec le MVC ajout des fichiers index.php et modele.php (en francais)

---
+++ html/affichageAcceuil.php
@@ -0,0 +1,40 @@
+<!-- Affichage -->
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+        <link href="style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <h1>Mon super blog !</h1>
+        <p>Derniers billets du blog :</p>
+
+
+        <?php
+        while ($donnees = $req->fetch())
+        {
+        ?>
+        <div class="news">
+            <h3>
+                <?php echo htmlspecialchars($donnees['title']); ?>
+                <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+            </h3>
+
+            <p>
+            <?php
+            echo nl2br(htmlspecialchars($donnees['content']));
+            ?>
+            <br />
+            <em><a href="#">Commentaires</a></em>
+            </p>
+        </div>
+        <?php
+        }
+        $req->closeCursor();
+        ?>
+    </body>
+</html>
--- html/index.php
+++ html/index.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<?php
+// Récupération des données
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=projectPhp;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
 
+$req = $bdd->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
 
-  </body>
-</html>
+require('affichageAcceuil.php');
+?>
