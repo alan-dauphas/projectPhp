fd2ec3dc1995e34d48add71955c9e035ceeaf622
alan-dauphas <alan.dauphas@me.com>
2018-10-18 15:28:37 +0200

    ajout d'une function de connexion a la base de données alias dbConnect() et renommage de la variable  en

--- html/modele.php
+++ html/modele.php
@@ -2,18 +2,23 @@
 
 function getBillet()
 {
-    try {
-      $bdd = new PDO('mysql:host=localhost;dbname=projectPhp;charset=utf8', 'root', '');
-    }
-    catch (Exception $e)
-    {
-      die('Erreur : ' . $e->getMessage());
-    }
 
-$req = $bdd->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+$req = dbConnect()->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
 
 return $req;
 
+}
+
+function dbConnect(){
+
+  try {
+    $db = new PDO('mysql:host=localhost;dbname=projectPhp;charset=utf8', 'root', '');
+    return $db;
+  }
+  catch (Exception $e)
+  {
+    die('Erreur : ' . $e->getMessage());
+  }
 }
 
 ?>
